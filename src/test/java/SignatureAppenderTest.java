1039e013f8ec0a89f68260cedab52f151d0711e6
Simon Huang <simon.huang@tum.de>
2022-03-30 17:25:31 +0200

    Add configuration test WIP

---
+++ src/test/java/SignatureAppenderTest.java
@@ -0,0 +1,48 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.builder.api.*;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class SignatureAppenderTest {
+
+    private static Logger logger;
+
+    private static BuiltConfiguration buildConfiguration() {
+
+        ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory
+                .newConfigurationBuilder();
+
+        AppenderComponentBuilder innerAppender = builder
+                .newAppender("console", "Console")
+                .add(builder.newLayout("PatternLayout")
+                        .addAttribute("pattern", "%sign %msg%n"));
+
+        AppenderComponentBuilder signatureAppender = builder
+                .newAppender("signatureAppender", "SignatureAppender")
+                .addAttribute("Appender", innerAppender);
+
+        RootLoggerComponentBuilder rootLogger = builder
+                .newRootLogger(Level.ERROR)
+                .add(builder.newAppenderRef("signatureAppender"));
+
+        builder.add(rootLogger);
+
+        return builder.build();
+    }
+
+    @BeforeAll
+    public static void setup() {
+
+        Configurator.initialize(buildConfiguration());
+        logger = LogManager.getLogger(SignatureAppenderTest.class);
+    }
+
+    @Test
+    public void configurationWorks() {
+        //TODO: Assert configuration working
+    }
+}
